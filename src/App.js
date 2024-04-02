c7dcead0371869a6f9aa58d486d8462d7049d603
Lambert Nnadi <40530031+lambo10@users.noreply.github.com>
2024-04-02 09:59:24 +0100

    new commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SpeedInsights } from "@vercel/speed-insights/react";
+
+function App() {
+  return (
+    <div>
+       <SpeedInsights />
+    </div>
+  );
+}
+
+export default App;
