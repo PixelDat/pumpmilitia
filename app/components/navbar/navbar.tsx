c16186f967e366acb60a2a8ae4ff0c90c752229d
simonayo <ayosimon.dev@gmail.com>
2024-04-08 13:08:38 +0100

    header section in progress

--- app/components/navbar/navbar.tsx
+++ app/components/navbar/navbar.tsx
@@ -1,58 +1,53 @@
-import { AppImages } from "@/lib/constants/app_images"
-import Image from "next/image"
+import { AppImages } from "@/lib/constants/app_images";
+import Image from "next/image";
+import "../../styles/navbar.css";
 
 const NavBar = () => {
-    return (
-        <nav className='px-16 flex flex-row items-center justify-between bg-black-leather-jacket-90 border border-black bg-opacity-75 backdrop-blur-[20px]'>
-         <div className="flex flex-row items-center">
-            <Image
-              src={AppImages.navBarLogo}
-              width={95}
-              height={95}
-              alt=""
-              priority />
+  return (
+    <nav className=" nav flex flex-row items-center justify-between mainnav">
+      <div className="flex flex-row items-center">
+        <Image
+          src={AppImages.navBarLogo}
+          width={95}
+          height={95.009}
+          alt=""
+          priority
+        />
 
+        <div className="font-gameria text-vivd-lime-green-10 text-[24px] nav-pump-text">
+          PUMP MILLITIA
+        </div>
+      </div>
 
-            <div className='font-gameria text-vivd-lime-green-10 text-[24px]'>
-                PUMP MILLITIA
-            </div>
-         </div>
+      <div className="flex flex-row space-x-8 items-center nav-links">
+        <div className="font-sans text-vivd-lime-green-10 text-[16px]">
+          <p>Airdrop</p>
+        </div>
 
+        <div className="font-sans text-vivd-lime-green-10 text-[16px]">
+          <p>Whitepaper</p>
+        </div>
 
-         <div className="flex flex-row space-x-8 items-center">
-           <div className='font-sans text-vivd-lime-green-10 text-[16px]'>
-                Airdrop
-            </div>
+        <div className="font-sans text-vivd-lime-green-10 text-[16px]">
+          <p>Presale</p>
+        </div>
 
-            <div className='font-sans text-vivd-lime-green-10 text-[16px]'>
-                Whitepaper
-            </div>
+        <div className="font-sans text-vivd-lime-green-10 text-[16px]">
+          <p>Roadmap</p>
+        </div>
 
-            <div className='font-sans text-vivd-lime-green-10 text-[16px]'>
-                Presale
-            </div>
+        <div className="font-sans text-vivd-lime-green-10 text-[16px]">
+          <p>Tokenomics</p>
+        </div>
 
-            <div className='font-sans text-vivd-lime-green-10 text-[16px]'>
-                Roadmap
-            </div>
+        <div className="font-sans text-vivd-lime-green-10 text-[16px]">
+          <p>Quests</p>
+        </div>
 
-            <div className='font-sans text-vivd-lime-green-10 text-[16px]'>
-                Tokenomics
-            </div>
+        <button className="navbar-auth-btn font-sans">Authentication</button>
+      </div>
+    </nav>
+  );
+};
 
-            <div className='font-sans text-vivd-lime-green-10 text-[16px]'>
-                Quests
-            </div>
-
-
-            <button className='bg-vivd-lime-green px-[24px] py-[12px] shadow-sm rounded-2xl shadow-white'>
-                Authentication
-            </button>
-         </div>
-        </nav>
-    )
-}
-
-
-
-export default NavBar
+export default NavBar;
