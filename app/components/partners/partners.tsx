345769f6bcc17c4237f97d17149540cda7de193b
Moses Erhinyodavwe <moseserhinyodavwe2@gmail.com>
2024-04-19 13:00:56 +0100

    I have started fixing the partners section and making it mobile responsive

--- app/components/partners/partners.tsx
+++ app/components/partners/partners.tsx
@@ -3,8 +3,8 @@ import Image from "next/image"
 
 const Partners = () => {
   return (
-    <div className="flex flex-col mx-auto w-7/12 items-center justify-center text-center my-24">
-      <div className="flex flex-col space-y-2">
+    <div className="flex flex-col mx-auto w-full items-center justify-center text-center my-24">
+      <div className="flex flex-col space-y-2 w-[680px] mb-3">
         <div className='font-gameria text-vivd-lime-green-10 text-[32px]'>
           PARTNERS
         </div>
@@ -14,72 +14,71 @@ const Partners = () => {
       </div>
 
 
-     <div className="flex flex-col">
-     <div className="flex flex-row items-center justify-center space-x-6">
-        <Image
-          className="max-h-[36px]"
-          src={AppImages.solana}
-          width={175}
-          height={36}
-          alt=""
-          priority />
+      <div className="flex flex-col items-center">
+        <div className="flex flex-row gap-x-20 w-full">
+          <Image
+            className="partner-images"
+            src={AppImages.solana}
+            width={100}
+            height={48}
+            alt=""
+            priority />
 
-        <Image
-                  className="max-h-[28px]"
-          src={AppImages.google}
-          width={175}
-          height={28}
-          alt=""
-          priority />
+          <Image
+            className="partner-images"
+            src={AppImages.google}
+            width={100}
+            height={48}
+            alt=""
+            priority />
 
-        <Image
-                          className="max-h-[48px]"
-          src={AppImages.stripe}
-          width={175}
-          height={48}
-          alt=""
-          priority />
+          <Image
+            className="partner-images"
+            src={AppImages.stripe}
+            width={100}
+            height={48}
+            alt=""
+            priority />
 
-        <Image
-                                  className="max-h-[40px]"
-          src={AppImages.aws}
-          width={175}
-          height={40}
-          alt=""
-          priority />
+          <Image
+            className="partner-images"
+            src={AppImages.aws}
+            width={100}
+            height={48}
+            alt=""
+            priority />
 
-        <Image
-        
-          src={AppImages.epic}
-          width={175}
-          height={36}
-          alt=""
-          priority />
+          <Image
+            className="partner-images"
+            src={AppImages.epic}
+            width={100}
+            height={48}
+            alt=""
+            priority />
+        </div>
+        <div className="flex flex-wrap">
+          <Image
+            src={AppImages.circleImage}
+            width={175}
+            height={36}
+            alt=""
+            priority />
+
+          <Image
+            src={AppImages.coinMarket}
+            width={175}
+            height={36}
+            alt=""
+            priority />
+
+          <Image
+            src={AppImages.galze}
+            width={175}
+            height={36}
+            alt=""
+            priority />
+        </div>
       </div>
-
-      <div className="flex flex-row items-center justify-center space-x-6">
-        <Image
-          src={AppImages.circleImage}
-          width={175}
-          height={36}
-          alt=""
-          priority />
-
-        <Image
-          src={AppImages.coinMarket}
-          width={175}
-          height={36}
-          alt=""
-          priority />
-
-        <Image
-          src={AppImages.galze}
-          width={175}
-          height={36}
-          alt=""
-          priority />
-      </div>
-     </div>
     </div>
   )
 }
