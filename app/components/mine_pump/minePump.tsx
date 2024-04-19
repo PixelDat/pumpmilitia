18db530c49f9bc397160307e9b6e1751ddc450e5
Moses Erhinyodavwe <moseserhinyodavwe2@gmail.com>
2024-04-19 10:12:35 +0100

    Working on the mobile responsiveness of the airdrop page and adding dynamics

--- app/components/mine_pump/minePump.tsx
+++ app/components/mine_pump/minePump.tsx
@@ -1,11 +1,15 @@
 import Image from "next/image";
 import { useState } from "react";
 import '../../styles/mine.css';
+import { KeyboardArrowLeftRounded, KeyboardArrowRightRounded } from "@mui/icons-material";
 
 
 const MinePump = () => {
   const [selectedItem, setSelectedItem] = useState(0);
 
+  function switchItems(index: number, button: string) {
+    console.log(index)
+  }
   const items = [
     {
       title: "Download Pump Millitia",
@@ -26,90 +30,187 @@ const MinePump = () => {
     }
   ]
   return (
-    <div
-      className="flex flex-col space-y-24 justify-center bg-cover bg-[url('/images/airdropbg.png')] items-center py-32"
-      id="airdrop"
-    >
-      <div className="flex flex-col items-center justify-center">
-        <div className="font-gameria text-vivd-lime-green-10 text-[40px]">
-          AIRDROP - MINE $PUMP
+    <>
+      {/* Big screens */}
+      <div
+        className="md:flex hidden  flex-col space-y-24 justify-center bg-cover bg-[url('/images/airdropbg.png')] items-center py-32"
+        id="airdrop"
+      >
+        <div className="flex flex-col items-center justify-center">
+          <div className="font-gameria text-vivd-lime-green-10 text-[40px]">
+            AIRDROP - MINE $PUMP
+          </div>
+
+          <div className="font-sans font-[700] text-vivd-lime-green-10 text-[16px] max-w-[800px] text-center">
+            You can start mining $PUMP tokens on your mobile phones right away.
+            We're opening the doors wide for every crypto enthusiast to join in on
+            the fun and rewards.
+          </div>
         </div>
 
-        <div className="font-sans font-[700] text-vivd-lime-green-10 text-[16px] max-w-[800px] text-center">
-          You can start mining $PUMP tokens on your mobile phones right away.
-          We're opening the doors wide for every crypto enthusiast to join in on
-          the fun and rewards.
-        </div>
-      </div>
+        <div data-aos="fade-down-right" data-aos-duration="1000" className="flex flex-row justify-center items-start space-x-24">
+          <div className="flex flex-col space-y-8 text-start max-w-[518px]">
+            <div className="mb-8" style={{ position: 'relative' }}>
 
-      <div data-aos="fade-down-right" data-aos-duration="1000" className="flex flex-row justify-center items-start space-x-24">
-        <div className="flex flex-col space-y-8 text-start max-w-[518px]">
-          <div className="mb-8" style={{ position: 'relative' }}>
+              <div className="loader">
 
-            <div className="loader">
-
+              </div>
+              <Image
+                style={{ position: 'absolute', top: -28, }}
+                src={"/svg/icon_logo_2.svg"}
+                width={69}
+                height={69}
+                alt=""
+                priority
+              />
             </div>
+
+
+            {items.map((item: any, index: number) => {
+              if (index !== selectedItem) {
+                return (
+                  <div onMouseEnter={() => { setSelectedItem(index) }} key={index} onClick={() => { setSelectedItem(index) }} className="flex flex-col justify-start items-start">
+                    <div className="font-gameria text-[#898989] text-[32px]">
+                      {item.title}
+                    </div>
+
+                    <div className="font-sans text-[#898989] text-[14px]">
+                      {item.sub_title}
+                    </div>
+
+                    <div className="border-b border-[#898989] w-full pt-2"></div>
+                  </div>)
+              } else {
+                return (
+                  <div onMouseEnter={() => { setSelectedItem(index) }} onClick={() => { setSelectedItem(index) }} className="flex flex-col justify-start items-start">
+                    <div className="font-gameria text-vivd-lime-green-10 text-[32px]">
+                      {item.title}
+                    </div>
+
+                    <div className="font-sans text-vivd-lime-green-10 text-[14px]">
+                      {item.sub_title}
+                    </div>
+
+                    <div className="border-b border-[#52594B] w-full pt-2"></div>
+                  </div>
+                )
+              }
+            })}
+
+            <div className="flex flex-row space-x-4 items-start justify-start">
+              <Image src={"/svg/play_store.svg"} width={185} height={56} alt="" />
+
+              <Image src={"/svg/app_store.svg"} width={185} height={56} alt="" />
+            </div>
+          </div>
+          <div data-aos="zoom-in-up" data-aos-duration="2000" className="">
             <Image
-              style={{ position: 'absolute', top: -28, }}
-              src={"/svg/icon_logo_2.svg"}
-              width={69}
-              height={69}
-              alt=""
+              src={items[selectedItem].image}
+              width={669}
+              height={446}
               priority
+              alt=""
             />
           </div>
+        </div>
+        <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+
+      </div>
 
 
-          {items.map((item: any, index: number) => {
-            if (index !== selectedItem) {
-              return (
-                <div onMouseEnter={() => { setSelectedItem(index) }} key={index} onClick={() => { setSelectedItem(index) }} className="flex flex-col justify-start items-start">
-                  <div className="font-gameria text-[#898989] text-[32px]">
-                    {item.title}
-                  </div>
+      {/* Small screens */}
+      <div
+        className="flex md:hidden flex-col space-y-24 justify-center bg-cover bg-[url('/images/airdropbg.png')] items-center py-32"
+        id="airdrop"
+      >
+        <div className="">
+          <div className="font-gameria text-center text-vivd-lime-green-10 text-[40px]">
+            AIRDROP - MINE $PUMP
+          </div>
 
-                  <div className="font-sans text-[#898989] text-[14px]">
-                    {item.sub_title}
-                  </div>
-
-                  <div className="border-b border-[#898989] w-full pt-2"></div>
-                </div>)
-            } else {
-              return (
-                <div onMouseEnter={() => { setSelectedItem(index) }} onClick={() => { setSelectedItem(index) }} className="flex flex-col justify-start items-start">
-                  <div className="font-gameria text-vivd-lime-green-10 text-[32px]">
-                    {item.title}
-                  </div>
-
-                  <div className="font-sans text-vivd-lime-green-10 text-[14px]">
-                    {item.sub_title}
-                  </div>
-
-                  <div className="border-b border-[#52594B] w-full pt-2"></div>
-                </div>
-              )
-            }
-          })}
-
-          <div className="flex flex-row space-x-4 items-start justify-start">
-            <Image src={"/svg/play_store.svg"} width={185} height={56} alt="" />
-
-            <Image src={"/svg/app_store.svg"} width={185} height={56} alt="" />
+          <div className="font-sans font-[700] p-3 leading-loose text-vivd-lime-green-10 text-[16px] max-w-[800px] text-center">
+            You can start mining $PUMP tokens on your mobile phones right away.
+            We're opening the doors wide for every crypto enthusiast to join in on
+            the fun and rewards.
           </div>
         </div>
-        <div data-aos="zoom-in-up" data-aos-duration="2000" className="">
-          <Image
-            src={items[selectedItem].image}
-            width={669}
-            height={446}
-            priority
-            alt=""
-          />
-        </div>
-      </div>
-      <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
 
-    </div>
+        <div
+          //  data-aos="fade-down-right" data-aos-duration="1000" 
+          className="">
+          <div className="flex flex-col text-start max-w-[518px]">
+            <div className="mx-4 mb-8" style={{ position: 'relative' }}>
+
+              <div className="loader">
+              </div>
+              <Image
+                style={{ position: 'absolute', top: -28, }}
+                src={"/svg/icon_logo_2.svg"}
+                width={69}
+                height={69}
+                alt=""
+                priority
+              />
+            </div>
+            {items.map((item: any, index: number) => {
+              if (selectedItem == index) {
+                return (
+                  <div onMouseEnter={() => { setSelectedItem(index) }} onClick={() => { setSelectedItem(index) }} className="flex flex-col p-4 justify-start items-start">
+                    <div
+                      //  data-aos="zoom-in-up" data-aos-duration="2000"
+                      className="mb-3">
+                      <Image
+                        src={item.image}
+                        width={669}
+                        height={446}
+                        priority
+                        alt=""
+                      />
+                    </div>
+
+                    <div className="font-gameria text-vivd-lime-green-10 text-[32px]">
+                      {item.title}
+                    </div>
+
+                    <div className="font-sans leading-loose text-vivd-lime-green-10 text-[14px]">
+                      {item.sub_title}
+                    </div>
+
+                    <div className="border-b border-[#52594B] w-full pt-2"></div>
+                    <div className="mt-8">
+                      <div className="flex flex-row gap-2">
+                        <KeyboardArrowLeftRounded onClick={() => {
+                          switchItems(index, 'forward')
+                        }} className="hover:bg-[#A5E314] bg-[none] text-[#979B93] hover:text-dark border border-[#979B93]" style={{ fontSize: '50px', borderRadius: '50%' }} />
+                        <KeyboardArrowRightRounded
+                          onClick={() => {
+                            switchItems(index, 'backward')
+                          }}
+                          className="" style={{ fontSize: '50px', background: 'white', borderRadius: '50%' }} />
+                      </div>
+                    </div>
+                  </div>
+                )
+              }
+            }
+            )
+            }
+
+
+
+
+            <div className="flex flex-row space-x-4 py-3 items-start justify-center">
+              <Image src={"/svg/play_store.svg"} width={153} height={45} alt="" />
+              <Image src={"/svg/app_store.svg"} width={153} height={45} alt="" />
+            </div>
+          </div>
+
+        </div>
+        <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+
+      </div>
+    </>
+
   );
 };
 
