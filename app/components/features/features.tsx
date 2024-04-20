e0f48030f1652198e1194767ec00e566506f5fbb
Lambert Nnadi <40530031+lambo10@users.noreply.github.com>
2024-04-20 14:36:03 +0100

    new commit

--- app/components/features/features.tsx
+++ app/components/features/features.tsx
@@ -86,7 +86,7 @@ const Features = () => {
         </div>
 
         <div
-          data-aos="zoom-in-up"
+          data-aos="zoom-out-up"
           className="feature-box relative">
           <Image
             src={'/images/feature3.png'}
