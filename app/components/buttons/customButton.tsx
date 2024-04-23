8010e1ffd6ca1beb3ca8c3831dcc9e50fb90d6bc
Moses Erhinyodavwe <moseserhinyodavwe2@gmail.com>
2024-04-23 16:01:58 +0100

    Currently working on the button components

--- app/components/buttons/customButton.tsx
+++ app/components/buttons/customButton.tsx
@@ -1,19 +1,20 @@
 import React from 'react';
 
 interface CustomButtonProps {
-    type: string;
+    type: 'normal' | 'transparent' | "gradient";
+    text: string;
     sx?: {};
     onChange?: React.ChangeEventHandler<HTMLInputElement>;
     disabled?: boolean;
     className?: string;
     addOnStart?: React.ReactNode;
     addOnEnd?: React.ReactNode;
-
     error?: boolean;
 }
 
 const CustomButton: React.FC<CustomButtonProps> = ({
     type,
+    text,
     className,
     onChange,
     addOnStart,
@@ -22,8 +23,22 @@ const CustomButton: React.FC<CustomButtonProps> = ({
     disabled
 }) => {
     return (
-        <div style={sx}>
-        </div>
+        <>
+            {type == "gradient" ?
+                <button className="bg-vivd-lime-green component_btn px-6 py-2 shadow-sm rounded-lg shadow-white">
+                    {text}
+                </button>
+                : type == "transparent" ?
+                    <button className="px-6 py-2 border component_btn_transparent border-vivd-lime-green rounded-lg text-vivd-lime-green-10">
+                        {text}
+                    </button>
+                    :
+                    <button className="px-6 py-2 border normal_btn border-vivd-lime-green rounded-lg ">
+                        {text}
+                    </button>
+            }
+        </>
+
 
     );
 }
--- app/pre-sale/page.tsx
+++ app/pre-sale/page.tsx
@@ -10,21 +10,33 @@ import Footer from "../components/footer/footer";
 import Onboarding from "../components/onboarding/onboarding";
 import { useEffect } from "react";
 import Tokenomics from "../components/tokenomics/tokennomics";
+import CustomButton from "../components/buttons/customButton";
 
 export default function Presale() {
   return (
     <div className="bg-cover bg-[url('/images/background.png')] h-full w-full">
-      <Hero />
 
-      <Preview />
-      <MinePump />
-      <Features />
-      <Onboarding />
-      <RoadMap />
-      <Tokenomics />
-      <Partners />
-      <Faqs />
-      <Footer />
+      <CustomButton type="gradient" text="Gradient Button" />
+
+      <br />
+      <br />
+      <CustomButton type="normal" text="Normal Button" />
+
+      <br />
+      <br />
+      <CustomButton type="transparent" text="Transparent Button" />
+
+      {/* <Hero /> */}
+      {/* <Preview /> */}
+      {/* <MinePump /> */}
+      {/* <Features /> */}
+      {/* <Onboarding /> */}
+      {/* <RoadMap /> */}
+      {/* <Tokenomics /> */}
+      {/* <Partners /> */}
+      {/* <Faqs /> */}
+      {/* <Footer /> */}
+
     </div>
   )
 }
