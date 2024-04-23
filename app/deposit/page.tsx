9eeeb1df1507f0902328abd4e408ecb173ba1d71
Moses Erhinyodavwe <moseserhinyodavwe2@gmail.com>
2024-04-23 11:56:27 +0100

    Currently working on the depostit page, adding dynamics

--- app/deposit/page.tsx
+++ app/deposit/page.tsx
@@ -12,123 +12,91 @@ import { People, Person, Person2Rounded } from "@mui/icons-material";
 export default function IndexPage() {
   let tasksCount = [
     {
-      number: 120,
-      image: '/svg/roadmap_1.svg',
-      status: 'Completed',
-      iconType: 'light',
+      title: 'connect your wallet',
+      image: '/images/deposit/connect.png',
+      subtitle: 'ComClick the “Connect” button to connect your Defi walletpleted',
     },
     {
-      number: 0,
-      image: '/svg/roadmap_2.svg',
-      status: 'Uncompleted',
-      iconType: 'light',
+      title: 'Enter Amount',
+      image: '/images/deposit/coins.png',
+      subtitle: 'Click the “Connect” button to connect your Defi wallet',
     },
-    {
-      number: 110,
-      image: '/svg/roadmap_3.svg',
-      status: 'Total',
-      iconType: 'light',
-    },
-
   ]
 
-  let tasks = [
-    {
-      image: '/images/tasksItem.png',
-      title: 'Follow @PUMPMILITIA ON',
-      icon: '/images/xicon.png',
-      reward: '100',
-    },
-    {
-      image: '/images/tasksItem.png',
-      title: 'Follow @PUMPMILITIA ON',
-      icon: '/images/xicon.png',
-      reward: '100',
-    },
-    {
-      image: '/images/tasksItem.png',
-      title: 'Follow @PUMPMILITIA ON',
-      icon: '/images/xicon.png',
-      reward: '100',
-    },
-    {
-      image: '/images/tasksItem.png',
-      title: 'Follow @PUMPMILITIA ON',
-      icon: '/images/xicon.png',
-      reward: '100',
-    },
-    {
-      image: '/images/tasksItem.png',
-      title: 'Follow @PUMPMILITIA ON',
-      icon: '/images/xicon.png',
-      reward: '100',
-    },
-    {
-      image: '/images/tasksItem.png',
-      title: 'Follow @PUMPMILITIA ON',
-      icon: '/images/xicon.png',
-      reward: '100',
-    }
-  ]
   const [completedTask, setCompletedTask] = useState(false)
   return (
     <div className="bg-cover bg-center overflow-hidden bg-[url('/images/deposit/depbag.png')] h-screen w-full">
       <NavBar />
-      <div className="pt-28 w-11/12 m-auto text-[#EDF9D0] font-kanit">
+      <div className="pt-64 w-11/12 m-auto text-[#EDF9D0] font-kanit">
+        <div className="flex flex-row gap-8 items-end">
+          <div className="basis-1/2 space-y-4">
+            <div className="">
+              <div className="flex flex-col gap-y-10 md:flex-row md:gap-x-5 pt-3">
+                {tasksCount.map((task, index) => {
+                  let gradient = index % 2 != 0 ? 'bg-gradient-to-t md:bg-gradient-to-b' : 'bg-gradient-to-t';
+                  return (
+                    <div className={`${gradient} basis-1/2 from-[#A5E314]/50 to-black flex flex-row justify-center p-0.5 rounded-3xl`}>
+                      <div key={`${index}-${task}`} className="h-[261px] relative px-[34.7px] py-[13px] w-full text-center space-y-8 md:text-start md:w-full bg-black/80 rounded-3xl">
+                        <div className="flex flex-row justify-center md:justify-start">
+                          <Image
+                            className=""
+                            src={task.image}
+                            width={80}
+                            height={80}
+                            priority
+                            alt="" />
+                        </div>
+                        <h4 className="text-[24px] font-gameria font-[500]">{task.title}</h4>
+                        <p className="text-[17px]">{task.subtitle}</p>
+                      </div>
+                    </div>
+                  )
+                })}
+              </div>
+            </div>
+            <div className="">
+              <div className="bg-gradient-to-t from-[#A5E314]/50 to-black p-0.5 rounded-3xl">
+                <div className="md:h-[261px] relative w-full bg-black/80 px-[24.8px] py-[24px] space-y-4  rounded-3xl">
+                  <Image
+                    className=""
+                    src={'/images/deposit/checkmark.png'}
+                    width={80}
+                    height={80}
+                    priority
+                    alt="" />
+                  <p className="text-[15px] font-[300]">Your balance today</p>
+                  <h4 className="text-[24px] font-gameria font-[500]">CONFIRM AND COMPLETE PURCHASE</h4>
+                  <p className="text-[15px] font-[300]">Click “Deposit” button and accept/approve transaction in your wallet. Wait for deposit to process</p>
 
-        {/* Dashboard Items */}
-        <div className="md:flex  md:flex-row  md:gap-x-14 px-4 md:w-11/12 m-auto  pb-20  items-start justify-between">
-          <div className="basis-1/4">
-            <div >
-              <h1 className="font-gameria text-[32px] mb-[24px]">$PUMP BALANCE</h1>
+                </div>
+              </div>
+
             </div>
 
-            <div className="bg-gradient-to-r from-[#A5E314]/50 to-black p-0.5 rounded-3xl">
-              <div className="md:h-[261px] relative w-full md:w-[411px] bg-black/80 px-[24.8px] py-[24px] space-y-4  rounded-3xl">
-                <p className="text-[15px] font-[300]">Your balance today</p>
-                <h4 className="text-[45px] font-gameria font-[500]">22,550,200</h4>
+          </div>
+          <div className="basis-1/2 relative text-black">
+
+            <div className="bg-[#D2F189] rounded-2xl h-[485px] p-4">
+
+              <div className="flex flex-row absolute w-full top-[-60px] justify-center">
                 <Image
-                  className="absolute top-[10px] right-[20px]"
-                  src={'/images/coins.png'}
-                  width={80}
-                  height={80}
+                  className=""
+                  src={'/images/deposit/pumpdep.png'}
+                  width={299}
+                  height={112}
                   priority
                   alt="" />
-                <div>
-                  <a href="/withdraw" className="navbar-auth-btn">Withdraw</a>
-                </div>
+              </div>
+              <div className="w-8/12 m-auto">
+                <p className="text-[16px] pt-10 text-center">Simply deposit from your wallet to fund your Pump Militia account and let the fun begin.</p>
+              </div>
 
-                <p className="text-[15px] font-[300]">Claim or stake tokens at TGE</p>
+              <div className="bg-[#20251a]">
 
               </div>
             </div>
+          </div>
 
-          </div>
-          <div className="basis-3/4">
-            <h1 className="font-gameria text-[24px] mb-[24px] text-start md:text-start">YOUR TASK</h1>
-            <div className="flex flex-col gap-y-10 md:flex-row md:gap-x-5 pt-3">
-              {tasksCount.map((task, index) => {
-                let gradient = index % 2 != 0 ? 'bg-gradient-to-t md:bg-gradient-to-b' : 'bg-gradient-to-t';
-                return (
-                  <div className={`${gradient} basis-1/3 from-[#A5E314]/50 to-black flex flex-row justify-center p-0.5 rounded-3xl`}>
-                    <div key={`${index}-${task}`} className="h-[261px] relative px-[34.7px] py-[13px] w-full text-center md:text-start md:w-full bg-black/80 rounded-3xl">
-                      <h4 className="text-[45px] font-[500]">{task.number}</h4>
-                      <p className="text-[17px]">{task.status}</p>
-                      <div className="flex flex-row justify-center md:justify-start">
-                        <Image
-                          className="absolute bottom-[10px]"
-                          src={task.image}
-                          width={100}
-                          height={100}
-                          priority
-                          alt="" />
-                      </div>
-                    </div>
-                  </div>
-                )
-              })}
-            </div>
-          </div>
         </div>
 
       </div>
