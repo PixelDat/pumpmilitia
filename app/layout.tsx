eb76440d01594a41e784f7ee97fa78e4b50d3d9f
Lambert Nnadi <40530031+lambo10@users.noreply.github.com>
2024-05-01 07:37:28 +0100

    Adjusted title, descriptions and site preload image for SEO optimisation

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,14 +40,14 @@ export default function RootLayout({ children }: Props) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="theme-color" content="#000000" />
 
-        <meta property="og:title" content="EverPump" />
-        <meta property="og:description" content="EverPump is a social experiment that rewards users for completing tasks on X(Twitter)" />
+        <meta property="og:title" content="Pump Militia" />
+        <meta property="og:description" content="Pump Militia is a blockchain-based game designed to merge thrilling gameplay with the innovative aspects of GameFi featuring competitive combat, strategic missions, and a vibrant community allowing players to earn real-world value through in-game achievements." />
         <meta property="og:image" content="https://everpump-6e275.web.app/sheared_external_imgs/sitethumnail.jpg" />
-        <meta property="og:url" content="https://everpump.io" />
+        <meta property="og:url" content="https://pumpmilitia.io" />
         <meta property="og:type" content="website" />
-        <meta name="description" content="EverPump is a social experiment that rewards users for completing tasks on X(Twitter)" />
+        <meta name="description" content="Pump Militia is a blockchain-based game designed to merge thrilling gameplay with the innovative aspects of GameFi featuring competitive combat, strategic missions, and a vibrant community allowing players to earn real-world value through in-game achievements." />
 
-        <link rel="apple-touch-icon" href="/images/logo192.png" />
+        <link rel="apple-touch-icon" href="favicon.ico" />
         <link rel="manifest" href="/files/manifest.json" />
         <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet"></link>
       </head>
