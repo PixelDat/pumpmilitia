bef14310bc0a72b4e7a6c69f71bd64c6fef7e1fa
Lambert Nnadi <40530031+lambo10@users.noreply.github.com>
2024-05-01 07:10:37 +0100

    new commit

---
+++ app/meta-data.tsx
@@ -0,0 +1,6 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'Pump Militia',
+  description: 'Pump Militia is a blockchain-based game designed to merge thrilling gameplay with the innovative aspects of GameFi featuring competitive combat, strategic missions, and a vibrant community allowing players to earn real-world value through in-game achievements.',
+};
