d943619f13919f8d9925201bd6977ae6db8fecb0
Moses Erhinyodavwe <moseserhinyodavwe2@gmail.com>
2024-06-04 10:21:04 +0100

    cached teh uniquId

--- app/telegram-loader/page.tsx
+++ app/telegram-loader/page.tsx
@@ -3,6 +3,8 @@ import Image from 'next/image'
 import { useSearchParams } from 'next/navigation';
 import React, { useEffect, useState } from 'react'
 import { authUser } from './utils';
+const Cookies = require("js-cookie");
+
 
 export default function TelegramBot() {
     const [loadingPage, setLoadingPage] = useState(true)
@@ -12,18 +14,10 @@ export default function TelegramBot() {
 
 
     useEffect(() => {
-        async function AuthUser() {
-            if (tg_username && uniqueId) {
-
-                const result = await authUser(tg_username, uniqueId)
-                console.log(result);
-                if (result?.status) {
-                    location.href = '/telegram-dash';
-                }
-            }
+        if (tg_username && uniqueId) {
+            Cookies.set("encrypt_id", uniqueId);
         }
-        AuthUser();
-    }, [])
+    }, [tg_username, uniqueId])
     return (
         <div className="bg-cover overflow-hidden bg-[url('/telegram/homepage.png')] h-screen w-screen" >
 
