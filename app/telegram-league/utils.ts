f5c61367058db1678231ca46683c33b0a48edfa0
Moses Erhinyodavwe <moseserhinyodavwe2@gmail.com>
2024-06-07 15:58:12 +0100

    Added the ranks of the leagues

--- app/telegram-league/utils.ts
+++ app/telegram-league/utils.ts
@@ -1,37 +1,44 @@
 export const leagues = [
     {
+        rank: "Corporal",
         title: 'League 1',
         image: '/telegram/league/coporal.png',
         from: '100,000',
     },
     {
+        rank: "Sergeant",
         title: 'League 2',
         image: '/telegram/league/sergent.png',
         from: '200,000',
 
     },
     {
+        rank: "Lieutenant",
         title: 'League 3',
         image: '/telegram/league/lieutenant.png',
         from: '400,000',
     },
     {
+        rank: "Captain",
         title: 'League 4',
         image: '/telegram/league/captain.png',
         from: '500,000',
     },
     {
+        rank: "Major",
         title: 'League 5',
         image: '/telegram/league/major.png',
         from: '1,000,000',
 
     },
     {
+        rank: "Colonel",
         title: 'League 6',
         image: '/telegram/league/colonel.png',
         from: '2,00,000',
     },
     {
+        rank: "General",
         title: 'League 7',
         image: '/telegram/league/general.png',
         from: '5,000,000',
