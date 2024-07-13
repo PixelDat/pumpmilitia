006b91e4f6c7f810f6c3be9cff7a71bb2e598026
Moses Erhinyodavwe <moseserhinyodavwe2@gmail.com>
2024-07-13 15:08:32 +0100

    service worker config

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,51 @@
+import { GenerateSW } from 'workbox-webpack-plugin';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.plugins.push(
+        new GenerateSW({
+          swDest: 'service-worker.js',
+          clientsClaim: true,
+          skipWaiting: true,
+          runtimeCaching: [
+            {
+              urlPattern: /^https?.*/,
+              handler: 'NetworkFirst',
+              options: {
+                cacheName: 'http-calls',
+                networkTimeoutSeconds: 15,
+                expiration: {
+                  maxEntries: 150,
+                  maxAgeSeconds: 30 * 24 * 60 * 60, // 1 month
+                },
+                cacheableResponse: {
+                  statuses: [0, 200],
+                },
+              },
+            },
+            {
+              urlPattern: /\.(?:html|json|txt)$/,
+              handler: 'StaleWhileRevalidate',
+              options: {
+                cacheName: 'static-texts',
+                expiration: {
+                  maxEntries: 50,
+                  maxAgeSeconds: 30 * 24 * 60 * 60, // 1 month
+                },
+              },
+            },
+          ],
+        })
+      );
+    }
+    return config;
+  },
   reactStrictMode: true,
   experimental: {
     missingSuspenseWithCSRBailout: false,
   },
 };
 
-
 export default nextConfig;
-
-
-
